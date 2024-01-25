1938c3ab902b3186926ea0471b81bd6bf98964ab
Ray <rayc12079@gmail.com>
2024-01-24 20:33:03 -0500

    Password visibility on account access is functional, added state to store form data values, firrebase auth not set up

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 import Google from "../Images/googleLogo.png"
 
 export default function Login() {
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
+  const changePasswordVisibility = () => {
+    setIsPasswordShown(prevState => !prevState)
+  }
+  const [formData, setFormData] = useState({email : "", password : ""})
+  const updateFormData = (e) => {
+    const {name, value} = e.target
+    setFormData(prevState => ({...prevState, [name] : value}))
+  }
+  console.log(formData)
   return (
     <section>
       <h1>Log In to an existing <br /> account.</h1>
       <form className='account--access--forms'>
         <label htmlFor="login--email">Email Address:</label>
-        <input type="text" placeholder='Email Address' id='login--email' required/>
+        <input name='email' onChange={updateFormData} type="text" placeholder='Email Address' id='login--email' required/>
         <label htmlFor="login--password">Password:</label>
-        <input type="password" placeholder='Password' id='login--password' required/>
-        <small className='understated'>Show Password</small>
+        <input name='password' onChange={updateFormData} type={isPasswordShown ? "text" : "password"} placeholder='Password' id='login--password' required/>
+        <small className='understated pointer' onClick={changePasswordVisibility}>{isPasswordShown ? "Hide Password" : "Show Password"}</small>
         <button className='google--account--access' type='button'><img src={Google} alt="Google Logo" height="16"/>Login With Google</button>
         <button className='cta expand'>Login</button>
       </form>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Google from "../Images/googleLogo.png"
 
 export default function Register() {
+  const [isPasswordShown, setIsPasswordShown] = useState(false)
+  const changePasswordVisibility = () => {
+    setIsPasswordShown(prevState => !prevState)
+  }
+  const [formData, setFormData] = useState({username : "", email : "", password : ""})
+  const updateFormData = (e) => {
+    const {name, value} = e.target
+    setFormData(prevState => ({...prevState, [name] : value}))
+  }
+  console.log(formData)
   return (
     <section>
       <h1>Register and Join us<br /> today.</h1>
       <form className='account--access--forms'>
-      <label htmlFor="login--email">Username:</label>
-        <input type="text" placeholder='Username' id='login--email' required/>
+        <label htmlFor="login--username">Username:</label>
+        <input name='username' onChange={updateFormData} type="text" placeholder='Username' id='login--username' required/>
         <label htmlFor="login--email">Email Address:</label>
-        <input type="text" placeholder='Email Address' id='login--email' required/>
+        <input name='email' onChange={updateFormData} type="text" placeholder='Email Address' id='login--email' required/>
         <label htmlFor="login--password">Password:</label>
-        <input type="password" placeholder='Password' id='login--password' required/>
-        <small className='understated'>Show Password</small>
+        <input name='password' onChange={updateFormData} type={isPasswordShown ? "text" : "password"} placeholder='Password' id='login--password' required/>
+        <small className='understated pointer' onClick={changePasswordVisibility}>{isPasswordShown ? "Hide Password" : "Show Password"}</small>
         <button className='google--account--access' type='button'><img src={Google} alt="Google Logo" height="25"/>Register With Google</button>
         <button className='cta expand'>Register</button>
       </form>
